abcbd0b73b4d1904e5570d4819ba5b6c2d0c186f
amritghimire <iamritghimire@gmail.com>
2016-08-09 10:32:45 +0545

    Some change including missing piece in main.py and other

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
-from flask_wtf import Form 
-from wtforms import StringField,BooleanField, PasswordField, validators, SubmitField, ValidationError
+from flask_wtf import Form
+from wtforms import StringField, BooleanField, PasswordField, validators, SubmitField, ValidationError
+
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Required()])
-    email = StringField('Email Address', [validators.Required()])
+    username = StringField('Username', [validators.DataRequired()])
+    email = StringField('Email Address', [validators.DataRequired()])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     test5 = BooleanField('I accept to all Terms & Conditions. ', [validators.DataRequired()])
-  
-#class UploadPic(Form):
+
+    # class UploadPic(Form):
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
-
-from flask import Flask, redirect, render_template,request
+from flask import Flask, redirect, render_template, request
 from forms import RegistrationForm
 
 app = Flask(__name__)
@@ -10,7 +9,9 @@ app = Flask(__name__)
 def hello():
     return redirect('/signmein')
 
+
 app.secret_key = 'you-will-never-guess'
+
 
 @app.route('/signmein', methods=['GET', 'POST'])
 def signmein():
@@ -20,6 +21,7 @@ def signmein():
     else:
         return render_template('signup.html', form=form)
 
+
 @app.route('/success')
 def success_signin():
     return 'Signin success'
@@ -27,10 +29,15 @@ def success_signin():
 
 @app.errorhandler(500)
 def server_error(e):
- # Log the error and stacktrace.
+    # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
 
 @app.errorhandler(404)
 def error404(error):
     return '<h3>Admit it,You Made a mistake.</h3><p>404</p>'
+
+
+if __name__ == '__main__':
+    app.run()
